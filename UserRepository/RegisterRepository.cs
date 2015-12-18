4a808883d38ab72af5efe6406b7725592e836de1
Joachim Flygare <joachimflygare@hotmail.com>
2015-12-18 14:40:49 +0100

    2:40

---
+++ UserRepository/RegisterRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+    class RegisterRepository
+    {
+        public static void Register(string firstname, string username, string gender, int age, string password, int visible)
+        {
+            using (var context = new datebookEntities())
+            {
+                Användare newUser = new Användare
+                {
+                    Namn = firstname,
+                    
+                 
+            }
+        }
+    }
+}
